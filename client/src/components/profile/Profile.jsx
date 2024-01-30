eb2f19f146a912670d1c7b9da2f2e432d31f86db
valderosch <phonelix96@gmail.com>
2024-01-31 00:57:26 +0200

    profile

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import defaultImage from '../../assets/img/user.png';
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {removeUserAvatar, uploadUserAvatar} from "../../actions/user";
 import {NavLink} from "react-router-dom";
+import './profile.css';
 
 const Profile = () => {
     const dispatch = useDispatch();
+    const user = useSelector(state => state.user.currentUser);
+    console.log(user);
+    const username = 'badmail';
 
 
     function onChangeHandler(e) {
@@ -17,11 +21,20 @@ const Profile = () => {
         <div className='profile'>
             <div className="profile__content">
                 <div className="avatar">
-                    <img src={defaultImage} alt="img" className='avatar__img'/>
-                    <button className="delete__btn" onClick={() => dispatch(removeUserAvatar())}>DELETE</button>
-                    <input accept="image/*" onChange={e => onChangeHandler(e)} type="file" className="upload__avatar" placeholder="Upload photo"/>
+                    <img src={defaultImage} alt="img" className='user__img'/>
+                    <div className="avatar__controls">
+                        <div className="title">Controls</div>
+                        <button className="delete__btn" onClick={() => dispatch(removeUserAvatar())}>Remove photo</button>
+                        <input accept="image/*" onChange={e => onChangeHandler(e)} type="file" className="upload__avatar" placeholder="Upload photo"/>
+                    </div>
                 </div>
-                <NavLink to='/'>HOME</NavLink>
+                <div className="info">
+                    <div className="info__email">Welcome back, {username}</div>
+                    <div className="info__files">FILES</div>
+                </div>
+                <div className="link">
+                    <NavLink to='/' className="navlink">MAIN</NavLink>
+                </div>
             </div>
         </div>
     );
