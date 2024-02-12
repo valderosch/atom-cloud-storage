2950a11ba9492581486770d87a579b809841a38f
valderosch <phonelix96@gmail.com>
2024-02-12 23:02:59 +0200

    profile stats

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -3,6 +3,7 @@ import defaultImage from '../../assets/img/user.png';
 import {useDispatch, useSelector} from "react-redux";
 import {removeUserAvatar, uploadUserAvatar} from "../../actions/user";
 import {calculatePercentage, formatFileSize} from "../disk/explorer/utility";
+import fileImg from '../../assets/img/context/download.png'
 import {NavLink} from "react-router-dom";
 import './profile.css';
 import {API_URL} from "../../config";
@@ -28,36 +29,90 @@ const Profile = () => {
         <div className='profile' onClick={() => setPopup(false)}>
             <div className="profile__content">
                 <div className="profile__data">
-                    <div className="avatar" onClick={event => event.stopPropagation()}>
-                        <img src={avatar} alt="img" className='user__img'/>
-                        <div className="edit-button" onClick={(e) => setPopup(!popup)}>🖉</div>
-                        {popup === true &&
-                            <div className="avatar__popup">
-                                <button className="delete__btn" onClick={() => dispatch(removeUserAvatar())}>Remove photo ⨯</button>
-                                <div className="avatar__handler">
-                                    <label htmlFor="upload__avatar" className="upload__avatar__label"> Upload Photo ⭱</label>
-                                    <input
-                                        accept="image/*"
-                                        onChange={e => onChangeHandler(e)}
-                                        type="file" id="upload__avatar"
-                                        className="upload__avatar"
-                                        placeholder="Upload photo"
-                                    />
+                    <div className="user-info">
+                        <div className="avatar" onClick={event => event.stopPropagation()}>
+                            <img src={avatar} alt="img" className='user__img'/>
+                            <div className="edit-button" onClick={(e) => setPopup(!popup)}>🖉</div>
+                            {popup === true &&
+                                <div className="avatar__popup">
+                                    <button className="delete__btn" onClick={() => dispatch(removeUserAvatar())}>Remove photo ⨯</button>
+                                    <div className="avatar__handler">
+                                        <label htmlFor="upload__avatar" className="upload__avatar__label"> Upload Photo ⭱</label>
+                                        <input
+                                            accept="image/*"
+                                            onChange={e => onChangeHandler(e)}
+                                            type="file" id="upload__avatar"
+                                            className="upload__avatar"
+                                            placeholder="Upload photo"
+                                        />
+                                    </div>
+                                </div>
+                            }
+                        </div>
+                        <div className="info">
+                            <div className="info__username">{name} 🙋‍♂️</div>
+                            <div className="info__block">
+                                <div className="info__email">Email: {user.email}</div>
+                                <div className="info__item">Your plan: <b>BASIC</b></div>
+                                <div className="info__item">Available space: <b>{maxsize}</b></div>
+                                <div className="info__item">Used Space: <b>{usedSpace}</b></div>
+                                <div className="info__bar__wrapper">
+                                    <div className="info__bar" style={{width: percentage + '%'}}></div>
+                                </div>
+                                <div className="info__loadstatus" style={{marginLeft: percentage-9.5 + '%'}}>👆<br/>You are here</div>
+                            </div>
+                        </div><br/>
+                    </div>
+                    <div className="stats_block">
+                        <div className="statistics">
+                            <div className="stats-title">/Statistic</div>
+                            <div className="stats-grid">
+                                <div className="stats-elem">
+                                    <div className="elem-title">Files 🛈</div>
+                                    <div className="elem-data">24</div>
+                                </div>
+                                <div className="stats-elem">
+                                    <div className="elem-title">Space 🛈</div>
+                                    <div className="elem-data">536 MB</div>
+                                </div>
+                                <div className="stats-elem">
+                                    <div className="elem-title">Folders 🛈</div>
+                                    <div className="elem-data">9</div>
+                                </div>
+                                <div className="stats-elem">
+                                    <div className="elem-title">Docs 📄</div>
+                                    <div className="elem-data">12 / 24</div>
+                                </div>
+                                <div className="stats-elem">
+                                    <div className="elem-title">Media 🌄</div>
+                                    <div className="elem-data">8 / 24</div>
+                                </div>
+                                <div className="stats-elem">
+                                    <div className="elem-title">Other 📊</div>
+                                    <div className="elem-data">4 / 24</div>
                                 </div>
                             </div>
-                        }
-                    </div>
-                    <div className="info">
-                        <div className="info__username">{name} 🙋‍♂️</div>
-                        <div className="info__block">
-                            <div className="info__email">Email: {user.email}</div>
-                            <div className="info__item">Your plan: <b>BASIC</b></div>
-                            <div className="info__item">Available space: <b>{maxsize}</b></div>
-                            <div className="info__item">Used Space: <b>{usedSpace}</b></div>
-                            <div className="info__bar__wrapper">
-                                <div className="info__bar" style={{width: percentage + '%'}}></div>
+                        </div>
+                        <div className="actions">
+                            <div className="stats-title">/Account</div>
+                            <div className="actions-block">
+                                <div className="user-story">
+                                    <div className="story-title">last added</div>
+                                    <div className="story-data">
+                                        <div className="story-item">
+                                            <img src={fileImg} alt="item" className="item-img"/>
+                                            <div className="item-filename">file.png</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="user-actions">
+                                    <div className="story-title">controls</div>
+                                    <div className="story-data">
+                                        ///
+                                        ///
+                                    </div>
+                                </div>
                             </div>
-                            <div className="info__loadstatus" style={{marginLeft: percentage-9.5 + '%'}}>👆<br/>You are here</div>
                         </div>
                     </div>
                 </div>
